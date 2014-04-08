6607ddc7a1d339eff4608f64cf3b257d736a2bf3
Jesus Calvo <jesus.calvo@gmail.com>
2014-04-08 20:34:55 +0200

    Added spring security

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,6 +44,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://repo.spring.io/milestone/"
     }
 
     dependencies {
@@ -67,6 +68,7 @@ grails.project.dependency.resolution = {
 		
 		compile ":joda-time:1.4"
         compile ":currencies:1.3.2"
+        compile ":spring-security-core:2.0-RC2"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
