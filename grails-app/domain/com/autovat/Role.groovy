86c1e916c1ad0ab9469992e9b2eeb0235bacf015
Jesus Calvo <jesus.calvo@gmail.com>
2014-12-01 22:34:44 +0100

    Upgrading to grails 2.4.4

---
+++ grails-app/domain/com/autovat/Role.groovy
@@ -0,0 +1,14 @@
+package com.autovat
+
+class Role {
+
+    String authority
+
+    static mapping = {
+        cache true
+    }
+
+    static constraints = {
+        authority blank: false, unique: true
+    }
+}
